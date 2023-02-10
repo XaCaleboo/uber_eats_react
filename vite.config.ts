8d852d8f001ec7eaa9d3627660dd31a05d9445ac
Анастасия Туманова <nastyk4t@gmail.com>
2023-02-10 19:46:06 +0400

    added ts and eslint plugins

--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,17 @@
 /* eslint-disable import/no-extraneous-dependencies */
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import checker from 'vite-plugin-checker'
 import path from 'path'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-	plugins: [react()],
+	plugins: [react(), checker({
+		eslint: {
+			lintCommand: 'eslint "./**/*.{ts,tsx}"',
+		},
+		typescript: true,
+	})],
 	resolve: {
 		alias: {
 			'@components': path.resolve(__dirname, './src/components'),
